2823664346a9f67ea5c490b45c8ad501f4f1946d
RohakV <rohak.v@gmail.com>
2020-06-13 16:17:42 +0100

    created login sytem and check

--- loginSystem/app.py
+++ loginSystem/app.py
@@ -1,22 +1,29 @@
 first_name = ""
 last_name = ""
-username = ""
+username = "a"
 email = ""
-password = ""
+password = "r"
 
 def route():
     getLoginInfo()
 
 def getLoginInfo():
     print("Welcome to login")
-    print("Remember, Everything is Case Sensitive!")
+    print("Remember, everything is case sensitive!")
     global l_uid
     global l_pswd
     l_uid = input("Enter your username: ")
     l_pswd = input("Enter your password: ")
+    check()
     
 def check():
-    
+    if l_uid == username:
+        if l_pswd == password:
+            print("You are in")
+        else:
+            print("wrong password")
+    else:
+        print("wrong username")
 
 
 route()
